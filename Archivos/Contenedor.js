838a82b301d67422524717e5edd42a5097232f8b
nicotim <nik21timo@gmail.com>
2022-04-01 17:35:56 -0300

    Trabajo en carpeta Archivos

--- Archivos/Contenedor.js
+++ Archivos/Contenedor.js
@@ -10,27 +10,45 @@ async function leerProductos() {
     }
 };
 
-leerProductos();
-
 
 class Contenedor {
 
-    constructor(title, price, thumbnail) {
-        this.title = title;
-        this.price = price;
-        this.thumbnail = thumbnail;
+    constructor(file) {
+        this.file = file;
     };
 
-    save(title, price, thumbnail) {
-        fs.writeFile('./Archivos/productos.txt', {"Titulo": title, "Precio": price, "Thumbnail": thumbnail}, error => {
-            if (err) {
-                console.log(`Hubo un error ${err}`)
+    save(object) {
+        let id = fs.length;
+        let newId = id + 1;
+        let newObject = object.add(id = newId)
+        fs.appendFile('./Archivos/productos.txt', newObject, error => {
+            if (error) {
+                console.log("No se pudo agregar el objeto al archivo");
             }else{
-                console.log('guardado!')
+                console.log("Guardado!")
             }
-        })    
-    }
+        }); 
+    };
     
+
+    getAll() {
+        return leerProductos();
+    };
+
+    deleteByID(number) {
+        let array = leerProductos();
+        array = array.filter(object => object.id != number);
+        return array;
+    };
+
+    deleteAll() {
+        let array = leerProductos();
+        let id = array.length;
+        let newId = id + 1;
+        array = array.filter(object => object.id != newId);
+        return array;
+    };
 };
 
-module.exports = Contenedor
+Contenedor.deleteAll();
+leerProductos()
--- Archivos/test.js
+++
@@ -1,4 +0,0 @@
-const Contenedor = require('./Contenedor.js')
-
-const producto = new Contenedor("Nike", 122, "https:/nike.com");
-console.log(producto.save())
